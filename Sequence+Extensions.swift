ba97c5b6cebc6fea11a18f42af4c07cbbf966cf3
Diego Torres <warorface@gmail.com>
2016-11-03 18:18:54 +0100

    Added Relative Iterator

--- Sequence+Extensions.swift
+++ Sequence+Extensions.swift
@@ -1,5 +1,41 @@
+private struct RelativeIterator<BaseElement>: IteratorProtocol {
+    
+    typealias RelativeTuple = (prev: BaseElement?, current: BaseElement, next: BaseElement?)
+    var previous: BaseElement? = nil
+    var nextElement: BaseElement?
+    let nextClosure: () -> BaseElement?
+    init<IteratorType: IteratorProtocol>(iterator: IteratorType) where IteratorType.Element == BaseElement {
+        var iterator = iterator
+        nextClosure = {
+            return iterator.next()
+        }
+        nextElement = iterator.next()
+        
+    }
+    
+    mutating func next() -> RelativeTuple? {
+        guard let current = nextElement else { return nil }
+        let next = nextClosure()
+        let tuple: RelativeTuple = (previous, current, next)
+        
+        previous = current
+        nextElement = next
+        
+        return tuple
+    }
+}
+
 extension Sequence {
-    typealias Element = Iterator.Element
+    typealias RelativeTuple = (prev: Iterator.Element?, current: Iterator.Element, next: Iterator.Element?)
+    
+    func relativeIterator() -> AnyIterator<RelativeTuple> {
+        let iterator = RelativeIterator(iterator: self.makeIterator())
+        let typeErasedIterator = AnyIterator(iterator)
+        return typeErasedIterator
+    }
+}
+
+extension Sequence {
     
     /**
      Calls the given closure for each element with the previous and next elements for relative context
@@ -11,10 +47,10 @@ extension Sequence {
         - next: The next element
      - complexity: O(n)
     */
-    func relativeEach( _ closure: (_ previous: Element?, _ current: Element, _ next: Element?) -> Void) {
+    func relativeEach( _ closure: (_ previous: Iterator.Element?, _ current: Iterator.Element, _ next: Iterator.Element?) -> Void) {
         var iterator = self.makeIterator()
-        var previous: Element? = nil
-        var nextElement: Element? = iterator.next()
+        var previous: Iterator.Element? = nil
+        var nextElement: Iterator.Element? = iterator.next()
         
         while let current = nextElement {
             let next = iterator.next()
